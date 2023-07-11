19997f405ce4f50d592d988c0e179fecddd73772
ariasz <129909951+szarias@users.noreply.github.com>
2023-07-11 19:35:00 -0300

    ter
    treter

---
+++ calculo imc/js/calculo.js
@@ -0,0 +1,11 @@
+let titulo = document.getElementById("title");
+
+titulo.innerHTML = "Veja o IMC";
+
+function imc(peso, altura){
+    let resultado = document.getElementById
+    ('Resultado')
+    resultado.innerText = (peso / (altura * altura))
+}
+
+let total = document.getElementById("total");
